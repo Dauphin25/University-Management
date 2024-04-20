701601702bfee096d8d1a068adaa1a29c1ea3870
Dauphin25 <roiofall@gmail.com>
2024-04-20 13:23:32 +0400

    making migrations

---
+++ management/models/__init__.py
@@ -0,0 +1,4 @@
+from management.models.student import Student
+from management.models.faculty import Faculty
+from management.models.professor import Professor
+from management.models.subject import Subject

fedb249df0bd82c36f4d751b0a27a2703f905f11
Dauphin25 <roiofall@gmail.com>
2024-04-20 13:22:34 +0400

    creating choices file

---
+++ management/choices.py
@@ -0,0 +1,11 @@
+SEMESTER_CHOICES = [
+    (0, 'Semester 0'),
+    (1, 'Semester 1'),
+    (2, 'Semester 2'),
+    (3, 'Semester 3'),
+    (4, 'Semester 4'),
+    (5, 'Semester 5'),
+    (6, 'Semester 6'),
+    (7, 'Semester 7'),
+    (8, 'Semester 8'),
+]

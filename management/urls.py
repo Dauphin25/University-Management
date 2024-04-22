dc5573dad1a6938c45eb3b735da7a85e3c3f6430
ikakopa1985 <ikakopa1985@gmail.com>
2024-04-22 17:27:24 +0400

    upload

--- management/urls.py
+++ management/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', index, name='index'),
     path('professor/', professor, name='professor'),
     path('sabject/', sabject, name='sabject'),
-    # path('professor/', professor, name='professor'),
+    path('takingsabject/', takingsabject, name='takingsabject'),
+    path('user_login/', user_login, name='user_login'),
+    path('user_logout/', user_logout, name='user_logout'),
     # path('professor/', professor, name='professor'),
 
 ]

5e94908dbc70b8c75213dd37041b28a80a927e64
ikakopa1985 <ikakopa1985@gmail.com>
2024-04-25 21:52:26 +0400

    added students photo

--- LMS/urls.py
+++ LMS/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('', include('management.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
